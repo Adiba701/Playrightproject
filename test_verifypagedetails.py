4047a522749fd356632c0a90a2cb5e315846e440
Adiba95 <adiba.nawaz@unthinkable.co>
2025-12-23 17:26:44 +0530

    code move

--- test_verifypagedetails.py
+++ test_verifypagedetails.py
@@ -4,8 +4,11 @@ from playwright.sync_api import expect
 def test_verifypagedetails(page):
     page.goto("https://www.orangehrm.com/")
     print("launch the url")
+
+    expect(page).not_to_have_title("Human Resources Management Software | HRMS | OrangeHRM")
     print(page.title())
+
     page.locator("xpath=(//a[@class='navbar-brand nav-logo'])[1]").is_visible()
-    print("verify page contains")
+    print("verify logo should be there")
 
 

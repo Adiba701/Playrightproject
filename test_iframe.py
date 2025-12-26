d52efb5c98c740381845847ee7f8df629b478530
Adiba95 <adiba.nawaz@unthinkable.co>
2025-12-26 15:44:13 +0530

    code move

--- test_iframe.py
+++ test_iframe.py
@@ -5,20 +5,20 @@ def test_iframe(page):
     # Launch website
     page.goto("https://the-internet.herokuapp.com/iframe")
 
-    # 1️⃣ Switch to iframe using frame_locator
+    # Switch to iframe using frame_locator
     iframe = page.frame_locator("#mce_0_ifr")
 
-    # 2️⃣ Clear existing content
+    # Clear existing content
     iframe.locator("body").click()
     page.keyboard.down("Control")
     page.keyboard.press("A")
     page.keyboard.up("Control")
     page.keyboard.press("Backspace")
 
-    # 3️⃣ Enter text
+    # Enter text
     iframe.locator("body").type("I rule the frames!")
 
-    # 4️⃣ Validate iframe content
+    # Validate iframe content
     expect(iframe.locator("body")).to_have_text("I rule the frames!")
 
     page.wait_for_timeout(3000)
